2ace717d9f87f931a31c4c4785a4a99f8e147ade
ilya.melnikov <ilya.melnikov@zyfra.com>
2021-12-07 12:37:27 +0300

    fix: :bug: disabled pprof

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -3,7 +3,6 @@ package api
 import (
 	"fmt"
 	"net/http"
-	"net/http/pprof"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -24,18 +23,18 @@ type Routes []Route
 
 var api controller.ApiController
 
-func AttachProfiler(router *mux.Router) {
-	router.HandleFunc("/debug/pprof/", pprof.Index)
-	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	router.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+// func AttachProfiler(router *mux.Router) {
+// 	router.HandleFunc("/debug/pprof/", pprof.Index)
+// 	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+// 	router.HandleFunc("/debug/pprof/profile", pprof.Profile)
+// 	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 
-	// Manually add support for paths linked to by index page at /debug/pprof/
-	router.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
-	router.Handle("/debug/pprof/heap", pprof.Handler("heap"))
-	router.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
-	router.Handle("/debug/pprof/block", pprof.Handler("block"))
-}
+// 	// Manually add support for paths linked to by index page at /debug/pprof/
+// 	router.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+// 	router.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+// 	router.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+// 	router.Handle("/debug/pprof/block", pprof.Handler("block"))
+// }
 
 func NewRouter(hc *healthcheck.HealthCheck) *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
@@ -51,7 +50,7 @@ func NewRouter(hc *healthcheck.HealthCheck) *mux.Router {
 			Handler(routerHandler)
 	}
 
-	AttachProfiler(router)
+	//AttachProfiler(router)
 
 	router.
 		Methods("GET").
